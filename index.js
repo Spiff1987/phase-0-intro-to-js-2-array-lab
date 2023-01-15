22696522512702f01b73ddc6275b61c5d8a69e1b
dismas <afubwa@gmail.com>
2023-01-15 11:29:46 +0400

    Javascript Array Lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+
+}
+function destructivelyRemoveLastCat(name){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift(name);
+}
+
+function appendCat(name) {
+    let catsCopy = cats.slice();
+    catsCopy.push(name);
+    return catsCopy;
+  }
+  
+  function prependCat(name) {
+    let catsCopy = [...cats];
+    catsCopy.unshift(name);
+    return catsCopy;
+  }
+  
+  function removeLastCat() {
+    let catsCopy = [...cats];
+    catsCopy.pop();
+    return catsCopy;
+  }
+  
+  function removeFirstCat() {
+    let catsCopy = cats.slice();
+    catsCopy.shift();
+    return catsCopy;
+  }
